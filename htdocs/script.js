e1520a23164b2661911de2cca02f1c75bd8f9b3d
KillianFvt <killian.fauvette@gmail.com>
2022-09-27 22:54:34 +0200

    Started Project
    Added :
    - TopNavBar
    - Navigation menu
    - Logo
    - Animations

---
+++ htdocs/script.js
@@ -0,0 +1,12 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+    document.getElementById("logoNav").style.top = "10px";
+  } else {
+    document.getElementById("navbar").style.top = "-75px";
+    document.getElementById("logoNav").style.top = "-90px";
+  }
+  prevScrollpos = currentScrollPos;
+}
